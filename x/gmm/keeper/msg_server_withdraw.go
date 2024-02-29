33f828cb269b0834b1eca4eccef656431bea1c0b
DedicatedDev <topdev323@gmail.com>
2024-02-29 12:02:23 +0200

    chore: remove pool forcely

--- x/gmm/keeper/msg_server_withdraw.go
+++ x/gmm/keeper/msg_server_withdraw.go
@@ -2,7 +2,6 @@ package keeper
 
 import (
 	"context"
-	"strings"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/sideprotocol/side/x/gmm/types"
@@ -16,46 +15,47 @@ func (k msgServer) Withdraw(goCtx context.Context, msg *types.MsgWithdraw) (*typ
 	}
 
 	pool, found := k.GetPool(ctx, msg.PoolId)
-	if !found {
-		return nil, types.ErrNotFoundAssetInPool
+	if found {
+		k.RemoveInterchainLiquidityPool(ctx, pool.PoolId)
+		//return nil, types.ErrNotFoundAssetInPool
 	}
 
-	outs, err := pool.EstimateWithdrawals(msg.Share)
-	if err != nil {
-		return nil, err
-	}
+	// outs, err := pool.EstimateWithdrawals(msg.Share)
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	// Check creator has enough share or not
-	bal := k.bankKeeper.GetBalance(ctx, sdk.MustAccAddressFromBech32(msg.Sender),
-		msg.Share.Denom,
-	)
-	if bal.IsLT(msg.Share) {
-		return nil, types.ErrInsufficientBalance
-	}
+	// // Check creator has enough share or not
+	// bal := k.bankKeeper.GetBalance(ctx, sdk.MustAccAddressFromBech32(msg.Sender),
+	// 	msg.Share.Denom,
+	// )
+	// if bal.IsLT(msg.Share) {
+	// 	return nil, types.ErrInsufficientBalance
+	// }
 
-	// Unlock asset from pool
-	if err = k.UnLockTokens(ctx, pool.PoolId, sdk.MustAccAddressFromBech32(msg.Receiver), outs); err != nil {
-		return nil, err
-	}
+	// // Unlock asset from pool
+	// if err = k.UnLockTokens(ctx, pool.PoolId, sdk.MustAccAddressFromBech32(msg.Receiver), outs); err != nil {
+	// 	return nil, err
+	// }
 
-	// Burn lp token
-	if err = k.BurnTokens(ctx, sdk.MustAccAddressFromBech32(msg.Sender), msg.Share); err != nil {
-		return nil, err
-	}
+	// // Burn lp token
+	// if err = k.BurnTokens(ctx, sdk.MustAccAddressFromBech32(msg.Sender), msg.Share); err != nil {
+	// 	return nil, err
+	// }
 
-	// Update pool statues.
-	if err := pool.DecreaseLiquidity(outs); err != nil {
-		return nil, err
-	}
-	pool.DecreaseShare(msg.Share.Amount)
+	// // Update pool statues.
+	// if err := pool.DecreaseLiquidity(outs); err != nil {
+	// 	return nil, err
+	// }
+	// pool.DecreaseShare(msg.Share.Amount)
 
-	// Save pool
-	k.SetPool(ctx, pool)
+	// // Save pool
+	// k.SetPool(ctx, pool)
 
-	rawOuts := []string{}
-	for _, out := range outs {
-		rawOuts = append(rawOuts, out.String())
-	}
+	// rawOuts := []string{}
+	// for _, out := range outs {
+	// 	rawOuts = append(rawOuts, out.String())
+	// }
 
 	ctx.EventManager().EmitEvent(
 		sdk.NewEvent(
@@ -68,10 +68,10 @@ func (k msgServer) Withdraw(goCtx context.Context, msg *types.MsgWithdraw) (*typ
 				Key:   types.AttributeKeyLpToken,
 				Value: msg.Share.String(),
 			},
-			sdk.Attribute{
-				Key:   types.AttributeKeyTokenOut,
-				Value: strings.Join(rawOuts, ":"),
-			},
+			// sdk.Attribute{
+			// 	Key:   types.AttributeKeyTokenOut,
+			// 	Value: strings.Join(rawOuts, ":"),
+			// },
 		),
 	)
 	return &types.MsgWithdrawResponse{}, nil
